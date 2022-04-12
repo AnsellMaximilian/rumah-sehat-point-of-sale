f0d600d9d29b47dc0e960840e5cc082da267c9fe
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-04-12 16:37:37 +0700

    add main routes with main layout

--- src/renderer/routes/index.tsx
+++ src/renderer/routes/index.tsx
@@ -1,14 +1,18 @@
 import {
   Routes as RouteList,
-  Route,
   MemoryRouter as Router,
+  Route,
 } from 'react-router-dom';
+import MainLayout from 'renderer/layout';
 
 const Routes = () => {
   return (
     <Router>
       <RouteList>
-        <Route path="/" element={<h1>test</h1>} />
+        <Route path="/" element={<MainLayout />}>
+          <Route path="/" element={<h1>TETS 1</h1>} />
+          <Route path="/fag" element={<h1>Fag 1</h1>} />
+        </Route>
       </RouteList>
     </Router>
   );
