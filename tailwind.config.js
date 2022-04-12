44f198dfa9fb819e1aee86b4a28361290d23a0ea
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-04-12 13:09:40 +0700

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/renderer/**/*.{js,jsx,ts,tsx,ejs}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {},
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
