c53357e3919e849118654c7faa245f93c5edeef7
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-04-13 12:12:20 +0700

    add primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        primary: '#15803D',
+      },
     },
   },
   variants: {
