45780a5bc0442365739f8319df1bceecfcd4383f
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-04-13 11:36:24 +0700

    Basic sidebar and close/open functionality

---
+++ src/renderer/layout/Sidebar/index.tsx
@@ -0,0 +1,14 @@
+const Sidebar = () => {
+  return (
+    <div className="bg-red-200">
+      <nav>
+        <ul>
+          <li>Dashboard</li>
+          <li>Settings</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/renderer/layout/index.tsx
+++ src/renderer/layout/index.tsx
@@ -1,10 +1,37 @@
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import Sidebar from './Sidebar';
 
 const MainLayout = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
   return (
-    <div>
-      <h1>This is layout</h1>
-      <Outlet />
+    <div className="flex">
+      <aside className="w-52">
+        <div
+          className={`fixed h-full w-52 transition-all duration-100 ${
+            isSidebarOpen ? '' : '-translate-x-52'
+          }`}
+        >
+          <Sidebar />
+        </div>
+      </aside>
+      <main className={`bg-green-200 grow ${isSidebarOpen ? '' : '-ml-52'}`}>
+        <header className="bg-yellow-200">
+          <div className="">
+            <button
+              type="button"
+              onClick={() => setIsSidebarOpen((state) => !state)}
+              className="bg-white"
+            >
+              Toggle
+            </button>
+          </div>
+        </header>
+        <div>
+          <Outlet />
+        </div>
+      </main>
     </div>
   );
 };
