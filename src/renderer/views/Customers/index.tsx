1d3bebcc8621c93732daacb3f32a46a2c63adaa3
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-04-23 11:09:25 +0700

    UI: Customer table

--- src/renderer/views/Customers/index.tsx
+++ src/renderer/views/Customers/index.tsx
@@ -18,9 +18,42 @@ const CustomersView = () => {
         </button>
       </div>
       <div>
-        {customers.map((cus) => (
-          <div>{cus.id}</div>
-        ))}
+        <table className="min-w-full divide-y divide-gray-200 shadow rounded-lg">
+          <thead className="bg-gray-50">
+            <tr>
+              <th className="uppercase font-medium text-left text-xs px-6 py-4">
+                Id
+              </th>
+              <th className="uppercase font-medium text-left text-xs px-6 py-4">
+                Name
+              </th>
+              <th className="uppercase font-medium text-left text-xs px-6 py-4">
+                Address
+              </th>
+              <th className="uppercase font-medium text-left text-xs px-6 py-4">
+                Phone
+              </th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-gray-200 bg-white">
+            {customers.map((cus) => (
+              <tr key={cus.id}>
+                <td className="px-6 py-4 text-sm text-cool-gray-900">
+                  {cus.id}
+                </td>
+                <td className="px-6 py-4 text-sm text-cool-gray-900">
+                  {cus.name}
+                </td>
+                <td className="px-6 py-4 text-sm text-cool-gray-900">
+                  {cus.address}
+                </td>
+                <td className="px-6 py-4 text-sm text-cool-gray-900">
+                  {cus.phone}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
