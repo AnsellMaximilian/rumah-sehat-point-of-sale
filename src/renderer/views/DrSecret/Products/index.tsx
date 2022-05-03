0b17d7c389c4109add61d06130dae5a07f8dff57
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-05-03 10:40:27 +0700

    Prepare sg product page for other components

--- src/renderer/views/DrSecret/Products/index.tsx
+++ src/renderer/views/DrSecret/Products/index.tsx
@@ -1,4 +1,53 @@
+import { useEffect, useState } from 'react';
+import confirm from 'renderer/utils/confirm';
+import DrSecretSGProduct, {
+  DrSecretSGProductCreateData,
+} from 'shared/types/dr-secret/DrSecretSGProduct';
+
 const Products = () => {
+  const [sgProducts, setSgProducts] = useState<DrSecretSGProduct[]>([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [sgProductToEdit, setCustomerToEdit] = useState<
+    DrSecretSGProduct | undefined
+  >(undefined);
+
+  useEffect(() => {
+    (async () => {
+      const allSgProducts = await window.electron.drSecret.sgProducts.read();
+      setSgProducts(allSgProducts);
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (!isModalOpen) setCustomerToEdit(undefined);
+  }, [isModalOpen]);
+
+  const deleteSgProduct = async (id: number) => {
+    confirm('Are you sure?', async () => {
+      await window.electron.drSecret.sgProducts.delete(id);
+      setSgProducts(await window.electron.drSecret.sgProducts.read());
+    });
+  };
+
+  const createSgProduct = async (customerData: DrSecretSGProductCreateData) => {
+    await window.electron.drSecret.sgProducts.create(customerData);
+    setIsModalOpen(false);
+    setSgProducts(await window.electron.drSecret.sgProducts.read());
+  };
+
+  const editSgProduct = (customer: DrSecretSGProduct) => {
+    setCustomerToEdit(customer);
+    setIsModalOpen(true);
+  };
+
+  const updateCustomer = async (
+    id: number,
+    customerData: DrSecretSGProductCreateData
+  ) => {
+    await window.electron.drSecret.sgProducts.update(id, customerData);
+    setIsModalOpen(false);
+    setSgProducts(await window.electron.drSecret.sgProducts.read());
+  };
   return (
     <div>
       <div className="flex justify-between mb-4">
