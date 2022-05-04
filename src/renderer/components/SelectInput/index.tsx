fe6acbca3e14b51271a21fa0ac135b6366b67fe8
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-05-04 11:51:37 +0700

    SelectInput finished

---
+++ src/renderer/components/SelectInput/index.tsx
@@ -0,0 +1,46 @@
+import { HTMLInputTypeAttribute } from 'react';
+
+interface Props {
+  label: string;
+  id: string;
+  value?: string | number | undefined;
+  containerClassName?: string | undefined;
+  inputClassName?: string | undefined;
+  onChange?: React.ChangeEventHandler<HTMLSelectElement>;
+  children: React.ReactNode;
+}
+
+const SelectInput = ({
+  label,
+  id,
+  value,
+  onChange,
+  containerClassName,
+  inputClassName,
+  children,
+}: Props) => {
+  return (
+    <div className={containerClassName}>
+      <label htmlFor={id} className="input-label">
+        {label}
+      </label>
+      <select
+        onChange={onChange}
+        className={`${inputClassName} input`}
+        id={id}
+        value={value}
+      >
+        {children}
+      </select>
+    </div>
+  );
+};
+
+SelectInput.defaultProps = {
+  onChange: undefined,
+  value: undefined,
+  containerClassName: undefined,
+  inputClassName: undefined,
+};
+
+export default SelectInput;
