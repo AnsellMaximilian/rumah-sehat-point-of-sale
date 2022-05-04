ea5b5909fcfe969e9af59d10dfd7ea1d53d77645
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-05-04 12:48:24 +0700

    SG invoice form: Add basic dynamic product row

--- src/renderer/views/DrSecret/SGInvoice/Form/index.tsx
+++ src/renderer/views/DrSecret/SGInvoice/Form/index.tsx
@@ -2,11 +2,21 @@ import TextInput from 'renderer/components/TextInput';
 import { useEffect, useState } from 'react';
 import SelectInput from 'renderer/components/SelectInput';
 import Customer from 'shared/types/Customer';
+import { DrSecretSGInvoiceItemCreateData } from 'shared/types/dr-secret/DrSecretSGInvoice';
+import { FaTrash } from 'react-icons/fa';
+import { v4 as uuidv4 } from 'uuid';
+
+interface WithReactKey {
+  key: string;
+}
 
 const SGInvoiceForm = () => {
   const [date, setDate] = useState('');
   const [customerId, setCustomerId] = useState<number | undefined>(undefined);
   const [customers, setCustomers] = useState<Customer[]>([]);
+  const [invoiceItems, setInvoiceItems] = useState<
+    Array<DrSecretSGInvoiceItemCreateData & WithReactKey>
+  >([]);
 
   useEffect(() => {
     (async () => {
@@ -15,6 +25,22 @@ const SGInvoiceForm = () => {
       if (allCustomers.length > 0) setCustomerId(allCustomers[0].id);
     })();
   }, []);
+
+  const addProduct = () => {
+    setInvoiceItems((prev) => {
+      return [
+        ...prev,
+        {
+          key: uuidv4(),
+          productId: 0,
+          points: 0,
+          priceSGD: 0,
+          quantity: 0,
+          deliveryFee: 0,
+        },
+      ];
+    });
+  };
 
   return (
     <div>
@@ -45,6 +71,48 @@ const SGInvoiceForm = () => {
             type="date"
             onChange={(e) => setDate(e.target.value)}
           />
+          <div className="col-span-12">
+            <div className="flex justify-between items-center mb-4">
+              <h2>Products</h2>
+              <button
+                className="btn-primary"
+                type="button"
+                onClick={addProduct}
+              >
+                Add Product
+              </button>
+            </div>
+            <table>
+              <thead>
+                <tr>
+                  <th>Product</th>
+                  <th>Points</th>
+                  <th>Price (SGD)</th>
+                  <th>Qty</th>
+                  <th>Points Subtotal</th>
+                  <th>Price Subtotal</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {invoiceItems.map((item) => (
+                  <tr key={item.key}>
+                    <td>{item.productId}</td>
+                    <td>{item.points}</td>
+                    <td>{item.priceSGD}</td>
+                    <td>{item.quantity}</td>
+                    <td>{item.quantity}</td>
+                    <td>{item.quantity}</td>
+                    <td className="flex gap-2">
+                      <button type="button" className="btn-danger p-2">
+                        <FaTrash />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </form>
       </div>
     </div>
