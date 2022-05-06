79baa102384429d0e24c41895b7278a908501900
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-05-06 14:57:52 +0700

    Replace product list in sg form with one in different file

--- src/renderer/views/DrSecret/SGInvoice/Form/index.tsx
+++ src/renderer/views/DrSecret/SGInvoice/Form/index.tsx
@@ -8,11 +8,11 @@ import {
 } from 'shared/types/dr-secret/DrSecretSGInvoice';
 
 import { deliveryFeeConstants } from 'shared/constants';
-import { FaTrash } from 'react-icons/fa';
 import { v4 as uuidv4 } from 'uuid';
 import DrSecretSGProduct from 'shared/types/dr-secret/DrSecretSGProduct';
 import RadioInput from 'renderer/components/RadioInput';
 import useSettings from 'renderer/hooks/useSettings';
+import ProductList from './ProductList';
 
 interface WithReactKey {
   key: string;
@@ -169,112 +169,14 @@ const SGInvoiceForm = () => {
             <span className="font-semibold">{exchangeRateSGDToRP.value}</span>
           </div>
           <div className="col-span-12">
-            <div className="flex justify-between items-center mb-4">
-              <h2>Products</h2>
-              <button
-                className="btn-primary"
-                type="button"
-                onClick={addProduct}
-              >
-                Add Product
-              </button>
-            </div>
-            <table>
-              <thead>
-                <tr>
-                  <th>Product</th>
-                  <th>Points</th>
-                  <th>Price (SGD)</th>
-                  {deliveryFeeMode === deliveryFeeConstants.INDIVIDUAL && (
-                    <th>Delivery</th>
-                  )}
-                  <th>Qty</th>
-                  <th>Points Subtotal</th>
-                  <th>Price Subtotal</th>
-                  {deliveryFeeMode === deliveryFeeConstants.INDIVIDUAL && (
-                    <th>Delivery Subtotal</th>
-                  )}
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {invoiceItems.map((item) => (
-                  <tr key={item.key}>
-                    <td>
-                      <SelectInput
-                        id={`invoice-item-product-${item.key}`}
-                        containerClassName="col-span-8"
-                        inputClassName="w-full"
-                        value={item.productId}
-                        onChange={(e) =>
-                          setInvoiceItems((prev) =>
-                            prev.map((prevItem) =>
-                              prevItem.key === item.key
-                                ? {
-                                    ...prevItem,
-                                    productId: Number(e.target.value),
-                                  }
-                                : prevItem
-                            )
-                          )
-                        }
-                      >
-                        {products.map((prod) => (
-                          <option key={prod.id} value={prod.id}>
-                            {prod.name}
-                          </option>
-                        ))}
-                      </SelectInput>
-                    </td>
-                    <td>{item.points}</td>
-                    <td>{item.priceSGD}</td>
-                    {deliveryFeeMode === deliveryFeeConstants.INDIVIDUAL && (
-                      <td>{item.deliveryFee}</td>
-                    )}
-                    <td>
-                      <TextInput
-                        id={`invoice-item-qty-${item.key}`}
-                        placeholder="Quantity"
-                        containerClassName="col-span-4"
-                        inputClassName="w-full"
-                        value={item.quantity}
-                        type="number"
-                        onChange={(e) =>
-                          setInvoiceItems((prev) =>
-                            prev.map((prevItem) =>
-                              prevItem.key === item.key
-                                ? {
-                                    ...prevItem,
-                                    quantity: Number(e.target.value),
-                                  }
-                                : prevItem
-                            )
-                          )
-                        }
-                      />
-                    </td>
-                    <td>{item.quantity * item.points}</td>
-                    <td>{item.quantity * item.priceSGD}</td>
-                    {deliveryFeeMode === 'individual' && (
-                      <td>
-                        {item.quantity *
-                          (item.deliveryFee ? item.deliveryFee : 0)}
-                      </td>
-                    )}
-
-                    <td className="flex gap-2">
-                      <button
-                        type="button"
-                        className="btn-danger p-2"
-                        onClick={() => deleteProduct(item.key)}
-                      >
-                        <FaTrash />
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+            <ProductList
+              products={products}
+              deleteProduct={deleteProduct}
+              addProduct={addProduct}
+              deliveryFeeMode={deliveryFeeMode}
+              invoiceItems={invoiceItems}
+              setInvoiceItems={setInvoiceItems}
+            />
           </div>
         </form>
       </div>
