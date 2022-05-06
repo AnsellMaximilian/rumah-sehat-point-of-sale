a3f2e5b66d5e5120d46c50ecc6ac2ce5333527ae
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-05-06 16:35:16 +0700

    Add filterYesOrNo

--- src/shared/helpers/index.tsx
+++ src/shared/helpers/index.tsx
@@ -1,9 +1,14 @@
+import { YesOrNo } from 'shared/types/general';
 import { DrSecretSGInvoiceDeliveryFeeMode } from '../types/dr-secret/DrSecretSGInvoice';
 
 export const filterDeliveryMode = (
   mode: string
 ): DrSecretSGInvoiceDeliveryFeeMode => {
   return mode === 'whole' || mode === 'individual' ? mode : 'individual';
+};
+
+export const filterYesOrNo = (answer: string): YesOrNo => {
+  return answer === 'yes' || answer === 'no' ? answer : 'no';
 };
 
 export default filterDeliveryMode;
